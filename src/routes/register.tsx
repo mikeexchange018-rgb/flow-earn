0716b5a14d31610581d27b347560146cecb8836d
mikeexchange018-rgb <mikeexchange018@gmail.com>
2026-09-09 17:03:40 +0100

    Fix import statement formatting in register.tsx

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -75,7 +75,8 @@ const Register = () => {
       </form>
     </div>
   )
-}import { createFileRoute } from '@tanstack/react-router'
+}
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/register')({
   component: Register,
