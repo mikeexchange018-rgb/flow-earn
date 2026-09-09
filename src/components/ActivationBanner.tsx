d9e1ef759866adfaa233c2bee6a193531d419ebf
mikeexchange018-rgb <mikeexchange018@gmail.com>
2026-09-09 18:03:03 +0100

    Change Link import from react-router-dom to @tanstack/react-router

--- src/components/ActivationBanner.tsx
+++ src/components/ActivationBanner.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "@/integrations/supabase/client";
-import { Link } from "react-router-dom";
+import { Link } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
 
 export const ActivationBanner = () => {
